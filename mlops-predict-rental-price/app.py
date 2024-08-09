63b0d90b8a4f0fe6c1adf811db6bfb2e692c650f
azonecloud <100863730+azonecloud@users.noreply.github.com>
2024-08-09 09:06:55 +0530

    Latest Commit

--- mlops-predict-rental-price/app.py
+++ mlops-predict-rental-price/app.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import numpy as np
+import joblib
 
 # Data Processing
 rentalDF = pd.read_csv('data/rental_1000.csv')
@@ -18,6 +19,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 # Model Training
 model = LinearRegression().fit(X_train, y_train)
 
+# Save the Model
+joblib.dump(model,'rental_price_model.joblib')
+
+# Load the Model
+model = joblib.load('rental_price_model.joblib')
+
 # Model Prediction
 y_pred = model.predict(X_test)
 
@@ -26,7 +33,17 @@ rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 print(f"RMSE: {rmse}")
 
 # Example prediction for a specific test sample
-sample_index = 0  # Change this index to test different samples
-predict_rental_price = model.predict([X_test[sample_index]])[0]
-print(f"The Real Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={y_test[sample_index]}")
-print(f"The Predicted Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={predict_rental_price}")
+#sample_index = 0  # Change this index to test different samples
+#predict_rental_price = model.predict([X_test[sample_index]])[0]
+#print(f"The Real Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={y_test[sample_index]}")
+#print(f"The Predicted Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={predict_rental_price}")
+
+rooms_count = int(input("Enter the number of rooms:="))
+area_sqft   = float(input("Enter the Area in Sqft:="))
+
+user_input = np.array([[rooms_count,area_sqft]])
+
+predict_rental_price = model.predict(user_input)[0]
+
+print(f"The Predicted Rental Price for Rooms count={rooms_count} and Area in Sqft={area_sqft} is={predict_rental_price}")
+
