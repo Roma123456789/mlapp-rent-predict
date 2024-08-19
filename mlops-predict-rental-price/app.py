7475246fadbe2d7bc2d084a4a28ae6fbd1550314
azonecloud <100863730+azonecloud@users.noreply.github.com>
2024-08-19 09:52:18 +0530

    Latest Commits

--- mlops-predict-rental-price/app.py
+++ mlops-predict-rental-price/app.py
@@ -28,23 +28,12 @@ model = joblib.load('rental_price_model.joblib')
 # Model Prediction
 y_pred = model.predict(X_test)
 
-# Compute RMSE
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-print(f"RMSE: {rmse}")
-
 # Example prediction for a specific test sample
 sample_index = 0  # Change this index to test different samples
 predict_rental_price = model.predict([X_test[sample_index]])[0]
 print(f"The Real Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={y_test[sample_index]}")
 print(f"The Predicted Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={predict_rental_price}")
 
-# User Inputs For Predictions
-# rooms_count = int(input("Enter the number of rooms:="))
-# area_sqft   = float(input("Enter the Area in Sqft:="))
-
-# user_input = np.array([[rooms_count,area_sqft]])
-
-# predict_rental_price = model.predict(user_input)[0]
-
-# print(f"The Predicted Rental Price for Rooms count={rooms_count} and Area in Sqft={area_sqft} is={predict_rental_price}")
-
+# Compute RMSE
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print(f"RMSE: {rmse}")
